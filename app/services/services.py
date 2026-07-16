7cfda5dfc578cce77ca8c5b6eae4c9222e2331d0
Nguyễn Thị Thu Hiền <nguyentth0909@gmail.com>
2026-07-16 19:58:36 +0700

    tt

--- app/services/services.py
+++ app/services/services.py
@@ -5,6 +5,7 @@ import app.schemas.schemas as schemas
 from sqlalchemy.exc import SQLAlchemyError
 from fastapi import HTTPException
 
+
 def create_warehouse(db: Session, warehouse: schemas.WarehouseCreate):
     try:
         new_warehouse = Warehouse(
@@ -19,8 +20,46 @@ def create_warehouse(db: Session, warehouse: schemas.WarehouseCreate):
         db.rollback()
         raise HTTPException(status_code=500, detail="Lỗi không xác định")
     
+
 def get_warehouse(db: Session, warehouse_id: int):
     result = db.query(Warehouse).filter(Warehouse.id == warehouse_id).first()
     if result is None:
         raise HTTPException(status_code=404, detail="ID không tồn tại")
     return result
+
+
+def update_package(db: Session, package_id: int, package_data: schemas.PackageUpdate):
+    db_package = db.query(Package).filter(Package.id == package_id).first()
+    if db_package is None:
+        raise HTTPException(
+            status_code=404, detail="Không tìm thấy kiện hàng với ID này")
+
+    update_data = package_data.model_dump(exclude_unset=True)
+
+    try:
+        for key, value in update_data.items():
+            setattr(db_package, key, value)
+        db.commit()
+        db.refresh(db_package)
+        return db_package
+    except SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(
+            status_code=500, detail="Lỗi không xác định khi cập nhật dữ liệu")
+
+
+def delete_waybill(db: Session, waybill_id: int):
+    db_waybill = db.query(Waybill).filter(Waybill.id == waybill_id).first()
+
+    if db_waybill is None:
+        raise HTTPException(
+            status_code=404, detail="Không tìm thấy vận đơn để xóa")
+
+    try:
+        db.delete(db_waybill)
+        db.commit()
+        return {"message": f"Xóa thành công vận đơn có ID {waybill_id}"}
+    except SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(
+            status_code=500, detail="Lỗi hệ thống không thể xóa vận đơn")
--- main.py
+++ main.py
@@ -18,3 +18,13 @@ def create_warehouse(warehouse: schemas.WarehouseCreate, db: Session = Depends(g
 @app.get("/warehouses/{warehouse_id}")
 def get_warehouse(warehouse_id: int, db: Session = Depends(get_db)):
     return services.get_warehouse(db, warehouse_id)
+
+
+@app.patch("/packages/{package_id}", response_model=schemas.PackageResponse, status_code=status.HTTP_200_OK)
+def update_package(package_id: int, package: schemas.PackageUpdate, db: Session = Depends(get_db)):
+    return services.update_package(db, package_id, package)
+
+
+@app.delete("/waybills/{waybill_id}", status_code=status.HTTP_200_OK)
+def delete_waybill(waybill_id: int, db: Session = Depends(get_db)):
+    return services.delete_waybill(db, waybill_id)
